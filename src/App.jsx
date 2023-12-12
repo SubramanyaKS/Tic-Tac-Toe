c0e9407062e861a3cd5cf3017e922bd52b049abe
Subramanya K S <subramanyaks2205@gmail.com>
2023-12-12 18:59:25 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/NavBar/Navbar'
 import { AiProvider } from './context/AIContext'
 import Home from './pages/Home'
 
 function App() {
   return (
     <AiProvider>
+      <Navbar/>
       <Home/>
     </AiProvider>
   )
