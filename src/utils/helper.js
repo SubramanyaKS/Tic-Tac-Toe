f44c829a4f6afdd937ecb7b647263516fa40108a
Subramanya K S <subramanyaks2205@gmail.com>
2023-12-22 07:04:25 +0530

    Added helper

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,3 +1,9 @@
+var ind1=0;
+var ind2=0;
+var ind3=0;
+export const winnerIndex=()=>{
+  return [ind1,ind2,ind3]
+}
 export const calculateWinner = (currentBoard) => {
     const lines = [
       [0, 1, 2],
@@ -13,7 +19,12 @@ export const calculateWinner = (currentBoard) => {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
+        // console.log("ABC",a,b,c)
+        ind1=a;
+        ind2=b;
+        ind3=c;
         return currentBoard[a];
+
       }
     }
 
